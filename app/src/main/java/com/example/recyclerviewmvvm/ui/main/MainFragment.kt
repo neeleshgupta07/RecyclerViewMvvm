c603b1a5c071f68841abede60416bfb7af7aacfa
neeleshgupta07 <neeleshgupta07@yahoo.co.in>
2020-07-06 23:12:17 +0530

    API integration

--- app/src/main/java/com/example/recyclerviewmvvm/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/recyclerviewmvvm/ui/main/MainFragment.kt
@@ -1,15 +1,22 @@
 package com.example.recyclerviewmvvm.ui.main
 
+import CountryInfo_Base
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.Nullable
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewmvvm.R
 
 class MainFragment : Fragment() {
-
+    protected lateinit var rootView: View
+    lateinit var recyclerView: RecyclerView
+    lateinit var adapter: CountryInfoAdapter
     companion object {
         fun newInstance() = MainFragment()
     }
@@ -18,13 +25,27 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        rootView =  inflater.inflate(R.layout.main_fragment, container, false);
+        initView()
+        return rootView
+    }
+    private fun initView(){
+        initializeRecyclerView()
     }
 
+    private fun initializeRecyclerView() {
+        recyclerView = rootView.findViewById(R.id.recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+    }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel.mutableData.observe(this, object: Observer<CountryInfo_Base> {
+            override fun onChanged(@Nullable mutableData:CountryInfo_Base) {
+                adapter = CountryInfoAdapter( mutableData.rows, this@MainFragment)
+                recyclerView.adapter = adapter
+            }
+        })
     }
 
 }
--- app/src/main/java/com/example/recyclerviewmvvm/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/recyclerviewmvvm/ui/main/MainViewModel.kt
@@ -1,7 +1,34 @@
 package com.example.recyclerviewmvvm.ui.main
 
+import CountryInfo_Base
+import Rows
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.recyclerviewmvvm.data.network.CountryApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    var mutableData:MutableLiveData<CountryInfo_Base> = MutableLiveData()
+    init{
+        // call  Rest API in init method
+        init()
+    }
+    private fun init() {
+        val apiInterface = CountryApi.invoke().getCountryInfo();
+        apiInterface.enqueue( object : Callback<CountryInfo_Base> {
+            override fun onResponse(call: Call<CountryInfo_Base>?, response: Response<CountryInfo_Base>?) {
+                if(response?.body() != null)
+                    mutableData.setValue(response.body())
+            }
+            override fun onFailure(call: Call<CountryInfo_Base>?, t: Throwable?) {
+
+            }
+        })
+
+    }
+
 }
